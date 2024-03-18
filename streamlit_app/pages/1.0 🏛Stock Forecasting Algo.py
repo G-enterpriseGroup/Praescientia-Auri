dacd6ea3fbbea0d3cfa12a000f7f5520435f9820
Presiraji <143067071+Presiraji@users.noreply.github.com>
2024-03-17 22:58:57 -0400

    Update 1.0 🏛Stock Forecasting Algo.py

--- streamlit_app/pages/1.0 🏛Stock Forecasting Algo.py
+++ streamlit_app/pages/1.0 🏛Stock Forecasting Algo.py
@@ -25,19 +25,14 @@ def hide_streamlit_branding():
 # Set the title of the app
 st.write("""
 # Forecasting Stock - Designed & Implemented by Raj Ghotra
-The code imports necessary libraries for data manipulation, visualization, and financial analysis. It acquires historical stock data, processes this data to calculate key financial indicators like moving averages and the MACD, a momentum indicator that shows the relationship between two moving averages of a security's price. The Signal line, a key part of MACD analysis, is also calculated along with a histogram that helps visualize the difference between the MACD and the Signal line.
 
-The financial indicators are plotted over time, allowing the user to assess the historical performance and trends of the stock. The historical data is then used to fit a predictive model, which is designed to forecast future values of these indicators based on past trends and patterns. The forecasting model is adjusted using sliders that control parameters such as seasonality and the smoothing factor of moving averages.
 
-The predictions made by this model can aid in identifying potential future movements in the stock's price, allowing for more informed decision-making in investment strategies. This could be particularly useful for tasks like optimizing entry and exit points in trading, managing portfolio risk, and conducting market analysis. By providing a visual representation of future predictions alongside historical data, the code facilitates a comparative analysis to evaluate the potential accuracy and effectiveness of the predictive model.
 
-# Seasonality "SN"
-In this code, seasonality refers to the recurrent fluctuations in stock prices that occur at regular intervals due to patterns in business activity, market sentiment, or other cyclical factors. The seasonality parameter, controlled by a slider, allows the user to specify the periodicity of the seasonal effects within the data—this could represent a typical pattern such as quarterly earnings reports, annual fiscal policies, or holiday shopping seasons, which are known to influence stock behavior.
 
-The user should judiciously set the seasonality parameter based on domain knowledge of the stock's behavior and the broader market. For instance, retail stocks may exhibit strong seasonal patterns around major holidays, while utility stocks might be more affected by seasonal changes in weather. Understanding and correctly setting this parameter can improve the model's predictive accuracy by aligning it more closely with the stock's intrinsic patterns.
 
-# Disclaimer
-The forecasting model presented operates strictly on historical quantitative data, and while it systematically analyzes past stock price movements and trends, it does not account for unforeseen events or new information. Such external factors include natural disasters, economic shifts, market sentiment, and significant news events, all of which can substantially impact stock prices. Therefore, while the model can provide insights into potential future stock behavior based on historical patterns, its predictions should not be solely relied upon for investment decisions. Users should consider it as one tool among many and integrate comprehensive market analysis, including qualitative factors, when evaluating investment opportunities.""")
+
+
+""")
 
 # Create sliders for each variable
 DD = 30
