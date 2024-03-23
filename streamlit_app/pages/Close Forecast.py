1c922a767c0b5581cc279f303ae8c330098541c2
Presiraji <143067071+Presiraji@users.noreply.github.com>
2024-03-23 14:21:43 -0400

    Update and rename Close & RSI.py to Close Forecast.py

--- streamlit_app/pages/Close Forecast.py
+++ streamlit_app/pages/Close Forecast.py
@@ -121,85 +121,18 @@ if st.button('Run SARIMAX Model'):
         df['MACD'] = df['EMA_19'] - df['EMA_39']
         # Assuming df['MACD'] is already calculated as shown in your code
         df['Signal'] = df['MACD'].ewm(span=9, adjust=False).mean()
-        df['Histogram'] = df['MACD'] - df['Signal']
         del df['EMA_19']
         del df['EMA_39']
         del df['EMA_9']
         progress_bar.progress(2)
 
 
-        df
-
-        import matplotlib.pyplot as plt
-        import matplotlib.dates as mdates
-        import pandas as pd
-
-        # Assuming 'df' is your DataFrame, and its index is of datetime type. If not, you've already converted it.
-        # Also assuming 'df' already has 'MACD', 'Signal', and 'Close' columns calculated.
-
-        # Sorting the DataFrame by the index (date) to ensure the data is in chronological order
-        df = df.sort_index()
-        progress_bar.progress(3)
-
-        # Calculate the Histogram if not already done
-        df['Histogram'] = df['MACD'] - df['Signal']
-
-        # Creating a figure and a grid of subplots
-        fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(14, 7))
-
-        # Plotting the Close price on ax1
-        ax1.plot(df.index, df['Close'], label='Close', color='skyblue')
-        ax1.set_title('Close Price')
-        ax1.legend(loc='upper left')
-        ax1.grid(True)
-
-        # Formatting the date to ensure that the date does not overlap
-        ax1.xaxis.set_major_locator(mdates.AutoDateLocator(minticks=5, maxticks=45))
-        ax1.xaxis.set_major_formatter(mdates.ConciseDateFormatter(ax1.xaxis.get_major_locator()))
-
-        # Plotting MACD and Signal Line on ax2
-
-        ax2.plot(df.index, df['MACD'], label='MACD', color='blue', linewidth=1.5)
-        ax2.plot(df.index, df['Signal'], label='Signal Line', color='red', linewidth=1.5)
-
-        # Plotting the Histogram as bar plot on ax2
-        ax2.bar(df.index, df['Histogram'], label='Histogram', color='grey', alpha=0.3)
-
-        ax2.set_title('MACD, Signal Line, and Histogram')
-        ax2.legend(loc='upper left')
-        ax2.grid(True)
-
-        # Formatting the date for the second subplot
-        ax2.xaxis.set_major_locator(mdates.AutoDateLocator(minticks=5, maxticks=45))
-        ax2.xaxis.set_major_formatter(mdates.ConciseDateFormatter(ax2.xaxis.get_major_locator()))
-
-        ax2.set_xlabel('Date')
-        ax2.set_ylabel('Value')
-
-        # Adjusting layout
-        plt.tight_layout()
-
-        # Show the plot
-        plt.show()
-
         C = df["Close"].dropna().tolist()
         M = df["MACD"].dropna().tolist()
         S = df["Signal"].dropna().tolist()
-        H = df["Histogram"].dropna().tolist()
-        progress_bar.progress(4)
-
         from statsmodels.tsa.arima.model import ARIMA
         from pmdarima import auto_arima
-        progress_bar.progress(5)
-
-        split_percentage = 0.80  # % for training
-        split_index = int(len(H) * split_percentage)
-        progress_bar.progress(6)
-
-        H_train = M[:split_index]
-        H_test = M[split_index:]
-        print(len(H_train), len(H_test))
-        progress_bar.progress(7)
+        progress_bar.progress(4)
 
         split_percentage = 0.80  # % for training
         split_index = int(len(C) * split_percentage)
@@ -210,45 +143,11 @@ if st.button('Run SARIMAX Model'):
         print(len(C_train), len(C_test))
         progress_bar.progress(9)
 
-        split_percentage = 0.80  # % for training
-        split_index = int(len(M) * split_percentage)
-        progress_bar.progress(10)
-
-        M_train = M[:split_index]
-        M_test = M[split_index:]
-        print(len(M_train), len(M_test))
-        progress_bar.progress(11)
-
-        split_percentage = 0.80  # % for training
-        split_index = int(len(S) * split_percentage)
-        progress_bar.progress(12)
-
-        S_train = S[:split_index]
-        S_test = S[split_index:]
-        print(len(S_train), len(S_test))
-        progress_bar.progress(13)
-
-        stepwise_fit = auto_arima(H,trace=True,suppress_warnings=True)
-        stepwise_fit
-        progress_bar.progress(14)
-
-        def extract_best_arima_order(stepwise_fit):
-            # Search for the line starting with "Best model:"
-            for line in stepwise_fit.split('\n'):
-                if line.startswith("Best model:"):
-                    # Extract numbers within parentheses
-                    order = tuple(map(int, line.split('ARIMA')[1].split('(')[1].split(')')[0].split(',')))
-                    return order
-            return None
-        arima_order = stepwise_fit.order
-        arima_order
-        hp, hd, hq = arima_order
-        progress_bar.progress(15)
 
         stepwise_fit = auto_arima(C,trace=True,suppress_warnings=True)
         stepwise_fit
         progress_bar.progress(16)
-
+        
         def extract_best_arima_order(stepwise_fit):
             # Search for the line starting with "Best model:"
             for line in stepwise_fit.split('\n'):
@@ -264,55 +163,7 @@ if st.button('Run SARIMAX Model'):
         
         stepwise_fit = auto_arima(M,trace=True,suppress_warnings=True)
         stepwise_fit
-        progress_bar.progress(18)
 
-        def extract_best_arima_order(stepwise_fit):
-            # Search for the line starting with "Best model:"
-            for line in stepwise_fit.split('\n'):
-                if line.startswith("Best model:"):
-                    # Extract numbers within parentheses
-                    order = tuple(map(int, line.split('ARIMA')[1].split('(')[1].split(')')[0].split(',')))
-                    return order
-            return None
-        arima_order = stepwise_fit.order
-        arima_order
-        mp, md, mq = arima_order
-        progress_bar.progress(19)
-
-        stepwise_fit = auto_arima(S,trace=True,suppress_warnings=True)
-        stepwise_fit
-        progress_bar.progress(20)
-
-        def extract_best_arima_order(stepwise_fit):
-            # Search for the line starting with "Best model:"
-            for line in stepwise_fit.split('\n'):
-                if line.startswith("Best model:"):
-                    # Extract numbers within parentheses
-                    order = tuple(map(int, line.split('ARIMA')[1].split('(')[1].split(')')[0].split(',')))
-                    return order
-            return None
-        arima_order = stepwise_fit.order
-        arima_order
-        sp, sd, sq = arima_order
-        progress_bar.progress(21)
-
-
-        import statsmodels.api as sm
-
-        arima_order = arima_order
-        seasonal_order = (hp, hd, hq, SN)  
-        progress_bar.progress(22)
-
-        model = sm.tsa.statespace.SARIMAX(H, order=arima_order, seasonal_order=seasonal_order)
-        model = model.fit()
-        progress_bar.progress(23)
-
-        start=len(H_train)
-        end=len(H_train)+len(C_test)-1
-        Hpred = model.predict(start=start,end=end)
-        progress_bar.progress(24)
-
-        Hpred_future = model.predict(start=end,end=end+DD)
         progress_bar.progress(25)
 
 
@@ -336,47 +187,6 @@ if st.button('Run SARIMAX Model'):
         progress_bar.progress(29)
 
 
-        import statsmodels.api as sm
-
-
-        arima_order = arima_order
-        seasonal_order = (mp, md, mq, SN)  
-        progress_bar.progress(30)
-
-        model = sm.tsa.statespace.SARIMAX(M, order=arima_order, seasonal_order=seasonal_order)
-        model = model.fit()
-        progress_bar.progress(31)
-
-        start=len(M_train)
-        end=len(M_train)+len(M_test)-1
-        Mpred = model.predict(start=start,end=end)
-        progress_bar.progress(32)
-
-
-        Mpred_future = model.predict(start=end,end=end+DD)
-        progress_bar.progress(33)
-
-
-
-        import statsmodels.api as sm
-
-        arima_order = arima_order
-        seasonal_order = (sp, sd, sq, SN)  
-        progress_bar.progress(34)
-
-        model = sm.tsa.statespace.SARIMAX(S, order=arima_order, seasonal_order=seasonal_order)
-        model = model.fit()
-        progress_bar.progress(35)
-
-        start=len(S_train)
-        end=len(S_train)+len(S_test)-1
-        Spred = model.predict(start=start,end=end)
-        progress_bar.progress(36)
-
-        Spred_future = model.predict(start=end,end=end+DD)
-        progress_bar.progress(37)
-
-
 
         # Assuming df.index is already set to datetime
         last_date = df.index[-2]  # Get the second to last date from the index
@@ -395,7 +205,7 @@ if st.button('Run SARIMAX Model'):
         Date
         progress_bar.progress(41)
 
-        df3 = pd.DataFrame({'Date':Date,'Cpred_future': Cpred_future, 'Mpred_future': Mpred_future, 'Spred_future': Spred_future, 'Hpred_future': Hpred_future})
+        df3 = pd.DataFrame({'Date':Date,'Cpred_future': Cpred_future})
         df3
         progress_bar.progress(42)
 
@@ -417,15 +227,6 @@ if st.button('Run SARIMAX Model'):
         fig, axs = plt.subplots(2, 1, figsize=(20, 12))  # Adjusts the figure size for two subplots
         progress_bar.progress(48)
 
-        # Plotting M and S predictions on the first subplot
-        axs[0].plot(df3.index, df3['Mpred_future'], label='M predictions', marker='o', color='blue')
-        axs[0].plot(df3.index, df3['Spred_future'], label='S predictions', marker='x', color='red')
-        axs[0].set_title(f'Zoomed Forecast MACD of {Ticker}')
-        axs[0].set_xlabel('Date')
-        axs[0].set_ylabel('Values')
-        axs[0].legend()
-        axs[0].grid(True)
-        axs[0].tick_params(axis='x', rotation=45)
 
         # Plotting Close Future predictions on the second subplot
         axs[1].plot(df3.index, df3['Cpred_future'], label='Close Future', marker='o', color='blue')
@@ -467,52 +268,33 @@ if st.button('Run SARIMAX Model'):
         df = df.sort_index()
         progress_bar.progress(51)
 
-        # Calculate the Histogram if not already done
-        df['Histogram'] = df['MACD'] - df['Signal']
-        progress_bar.progress(52)
-
-        # Creating a figure and a grid of subplots
-        fig, axs = plt.subplots(5, 1, figsize=(14.875, 19.25), dpi=300)
+        fig, axs = plt.subplots(1, 1, figsize=(14.875, 19.25), dpi=300)
         fig.suptitle(f"{Ticker}-Data Used for Forecasting {start_date1} to {today} for {DD} Days Forecast", fontsize=25, y=.99)
         # Plotting the Close price on axs[0]
-        axs[4].plot(df.index, df['Close'], label='Close', color='Black')
-        axs[4].set_title('Close Price')
-        axs[4].legend(loc='upper left')
-        axs[4].grid(True)
-
-        # Formatting the date to ensure that the date does not overlap
-        axs[4].xaxis.set_major_locator(mdates.AutoDateLocator(minticks=5, maxticks=45))
-        axs[4].xaxis.set_major_formatter(mdates.ConciseDateFormatter(axs[0].xaxis.get_major_locator()))
-        # Plotting MACD and Signal Line on axs[1]
-        axs[1].plot(df.index, df['MACD'], label='MACD', color='blue', linewidth=1.5)
-        axs[1].plot(df.index, df['Signal'], label='Signal Line', color='red', linewidth=1.5)
-        # Plotting the Histogram as bar plot on axs[1]
-        axs[1].bar(df.index, df['Histogram'], label='Histogram', color='grey', alpha=0.3)
-        axs[1].set_title('MACD, Signal Line, and Histogram')
+        axs[1].plot(df.index, df['Close'], label='Close', color='Black')
+        axs[1].set_title('Close Price')
         axs[1].legend(loc='upper left')
         axs[1].grid(True)
-        # Formatting the date for the second subplot
+        
+        # Formatting the date to ensure that the date does not overlap
         axs[1].xaxis.set_major_locator(mdates.AutoDateLocator(minticks=5, maxticks=45))
-        axs[1].xaxis.set_major_formatter(mdates.ConciseDateFormatter(axs[1].xaxis.get_major_locator()))
-        # Forecast plots
-        # MACD and Signal future predictions
-        axs[2].plot(df.index, df['MACD'], label='MACD', color='blue')
-        axs[2].plot(df.index, df['Signal'], label='Signal', color='red')
-        axs[2].plot(df3.index[-1000:], df3['Mpred_future'][-1000:], label='MACD Future', linestyle='--', color='blue')
-        axs[2].plot(df3.index[-1000:], df3['Spred_future'][-1000:], label='Signal Future', linestyle='--', color='red')
-        axs[2].set_title('Forecast MACD and Signal Line')
-        axs[2].legend()
+        axs[1].xaxis.set_major_formatter(mdates.ConciseDateFormatter(axs[0].xaxis.get_major_locator()))
+
+        
+ 
+        
+        # Formatting the date for the second subplot
+        axs[0].xaxis.set_major_locator(mdates.AutoDateLocator(minticks=5, maxticks=45))
+        axs[0].xaxis.set_major_formatter(mdates.ConciseDateFormatter(axs[0].xaxis.get_major_locator()))
+        
+
+        
         # Closing price and future prediction
-        axs[3].plot(df.index, df['Close'], label='Closed', color='Black')
-        axs[3].plot(df3.index[-1000:], df3['Cpred_future'][-1000:], label='Closing Future', linestyle='--', color='Blue')
-        axs[3].set_title('Forecast Closing Price')
-        axs[3].legend()
-        # Histogram and future prediction
-        width = 0.22
-        axs[0].bar(df.index, df['Histogram'], width, label='Histogram', color='blue')
-        axs[0].bar(df3.index[-1000:], df3['Hpred_future'][-1000:], width, label='Histogram Future', color='green')
-        axs[0].set_title('Forecast Histogram')
+        axs[0].plot(df.index, df['Close'], label='Closed', color='Black')
+        axs[0].plot(df3.index[-1000:], df3['Cpred_future'][-1000:], label='Closing Future', linestyle='--', color='Blue')
+        axs[0].set_title('Forecast Closing Price')
         axs[0].legend()
+        
         # General settings for all subplots
         for ax in axs:
             ax.xaxis.set_major_locator(mdates.AutoDateLocator())
@@ -536,14 +318,14 @@ if st.button('Run SARIMAX Model'):
                 )  
         progress_bar.progress(100)
         st.success("Model run successfully!")
-
-
-
-
-
-
-
-
+        
+        
+        
+        
+        
+        
+        
+        
 
 #_______________________________________________________________________________________________________________________________________________________________
 # Function to fetch data
