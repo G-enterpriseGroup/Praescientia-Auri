ec01a2a869d77fe540e58ec27cf1ae9900a2b24e
Presiraji <143067071+Presiraji@users.noreply.github.com>
2024-04-07 17:36:34 -0400

    Update Pool.py

--- streamlit_app/pages/Pool.py
+++ streamlit_app/pages/Pool.py
@@ -1,20 +1,20 @@
 import streamlit as st
 
-# Title
+# Title of the application
 st.title('Pool Chemical Dosage Calculator')
 
-# User Inputs
-pool_volume = st.number_input('Enter your pool volume (in gallons):', min_value=0.0, format='%f')
-desired_change = st.number_input('Enter the desired chemical change (ppm):', min_value=0.0, format='%f')
-chemical_label_amount = st.number_input('Enter the amount of chemical from the label (in lbs or oz):', min_value=0.0, format='%f')
-given_water_volume = st.number_input('Enter the given water volume from the label (in gallons):', min_value=0.0, format='%f')
-given_chemical_change = st.number_input('Enter the given chemical change from the label (ppm):', min_value=0.0, format='%f')
+# Input fields for the user to provide necessary data
+pool_volume = st.number_input('Enter your pool volume (in gallons):', min_value=0.0, step=1000.0, format='%f')
+desired_change = st.number_input('Enter the desired chemical change (in ppm):', min_value=0.0, step=1.0, format='%f')
+chemical_label_amount = st.number_input('Enter the amount of chemical from the label (in lbs):', min_value=0.0, step=0.1, format='%f')
+given_water_volume = st.number_input('Enter the given water volume from the label (in gallons):', min_value=0.0, step=1000.0, format='%f')
+given_chemical_change = st.number_input('Enter the given chemical change from the label (in ppm):', min_value=0.0, step=1.0, format='%f')
 
-# Calculation
+# Button to trigger the calculation
 if st.button('Calculate Dosage'):
+    # Calculation logic
     pool_factor = pool_volume / given_water_volume
     change_factor = desired_change / given_chemical_change
     chemical_dosage = pool_factor * change_factor * chemical_label_amount
-    st.write(f'You need {chemical_dosage:.2f} lbs or oz of the chemical for your pool.')
-
-
+    # Display the result
+    st.write(f'You need {chemical_dosage:.2f} lbs of the chemical for your pool.')
