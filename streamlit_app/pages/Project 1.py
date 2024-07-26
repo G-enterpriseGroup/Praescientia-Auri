737f4f00d03e89be1aef073dfa2e783cad483cbd
Presiraji <143067071+Presiraji@users.noreply.github.com>
2024-07-25 20:31:23 -0400

    Update Project 1.py

--- streamlit_app/pages/Project 1.py
+++ streamlit_app/pages/Project 1.py
@@ -1,7 +1,6 @@
 import streamlit as st
 from selenium import webdriver
-from selenium.webdriver.common.by
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
