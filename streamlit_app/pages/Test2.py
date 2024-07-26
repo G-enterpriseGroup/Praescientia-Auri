f4c697b4429763b5512d8cb139d8bb7b03ab81ed
Presiraji <143067071+Presiraji@users.noreply.github.com>
2024-07-25 22:24:20 -0400

    Update Test2.py

--- streamlit_app/pages/Test2.py
+++ streamlit_app/pages/Test2.py
@@ -13,6 +13,9 @@ def fetch_tickers(url):
 
     return symbols
 
+def clean_tickers(tickers):
+    return [ticker.split(':')[1] if ':' in ticker else ticker for ticker in tickers]
+
 # URL of the HTML file
 url = 'https://www.tradingview.com/watchlists/139248623/'  # Replace with the actual URL of your HTML file
 
@@ -20,10 +23,11 @@ st.title("Ticker List")
 st.write("Fetching tickers from HTML file...")
 
 tickers = fetch_tickers(url)
+cleaned_tickers = clean_tickers(tickers)
 
-if tickers:
+if cleaned_tickers:
     st.write("Tickers found:")
-    tickers_str = ", ".join(tickers)
+    tickers_str = ", ".join(cleaned_tickers)
     st.write(tickers_str)
 else:
     st.write("No tickers found.")
