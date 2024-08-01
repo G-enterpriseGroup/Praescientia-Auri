c7e4d1599aaeafeb69c9c6ffa80cd3d8be659c6f
G-enterpriseGroup <143067071+G-enterpriseGroup@users.noreply.github.com>
2024-08-01 17:41:05 -0400

    Update Testing Phase 2.py

--- streamlit_app/pages/Testing Phase 2.py
+++ streamlit_app/pages/Testing Phase 2.py
@@ -8,7 +8,7 @@ def get_stock_data(ticker):
     base_url = "https://stockanalysis.com"
     etf_url = f"{base_url}/etf/{ticker}/dividend/"
     stock_url = f"{base_url}/stocks/{ticker}/dividend/"
-    tradingview_url = f"https://www.tradingview.com/symbols/NYSE-{ticker}/"
+    tradingview_url = f"https://www.tradingview.com/symbols/{ticker}/"
 
     try:
         response = requests.get(etf_url)
