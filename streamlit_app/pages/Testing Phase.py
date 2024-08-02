f1475bc0f8d301e9ada71ed2b4ca1f2eb939d6fc
G-enterpriseGroup <143067071+G-enterpriseGroup@users.noreply.github.com>
2024-08-01 21:37:47 -0400

    Update Testing Phase.py

--- streamlit_app/pages/Testing Phase.py
+++ streamlit_app/pages/Testing Phase.py
@@ -95,7 +95,7 @@ if tickers:
 
     # Display performance dates
     st.header("Performance Dates")
-    dates_table = {period: date.strftime('%m-%d-%yy') for period, date in periods_data.items()}
+    dates_table = {period: date.strftime('%m-%d-20%y') for period, date in periods_data.items()}
     st.table(dates_table)
 
 # Adjust the width and height of the page and ensure table fits the data
