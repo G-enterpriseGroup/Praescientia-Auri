5c241b09f1eb3733a02235d84876e49a3fa8769a
G-enterpriseGroup <143067071+G-enterpriseGroup@users.noreply.github.com>
2024-08-03 01:28:31 -0400

    Update Testing Phase ETF.py

--- streamlit_app/pages/Testing Phase ETF.py
+++ streamlit_app/pages/Testing Phase ETF.py
@@ -20,7 +20,11 @@ def get_stock_data(ticker):
             ex_dividend_date = tree.xpath('/html/body/div/div[1]/div[2]/main/div[2]/div/div[2]/div[3]/div/text()')[0].strip()
             frequency = tree.xpath('//*[@id="main"]/div[2]/div/div[2]/div[4]/div/text()')[0].strip()
             dividend_growth = tree.xpath('/html/body/div/div[1]/div[2]/main/div[2]/div/div[2]/div[6]/div/text()')[0].strip()
-            return {"Ticker": ticker, "Price": price, "Yield %": yield_percent, "Annual Dividend": annual_dividend, "Ex Dividend Date": ex_dividend_date, "Frequency": frequency, "Dividend Growth %": dividend_growth}
+            return {
+                "Ticker": ticker, "Price": price, "Yield %": yield_percent,
+                "Annual Dividend": annual_dividend, "Ex Dividend Date": ex_dividend_date,
+                "Frequency": frequency, "Dividend Growth %": dividend_growth
+            }
         else:
             # If stock URL fails, attempt to fetch ETF data
             response = requests.get(etf_url)
@@ -32,54 +36,88 @@ def get_stock_data(ticker):
                 ex_dividend_date = tree.xpath('/html/body/div/div[1]/div[2]/main/div[2]/div/div[2]/div[3]/div/text()')[0].strip()
                 frequency = tree.xpath('//*[@id="main"]/div[2]/div/div[2]/div[4]/div/text()')[0].strip()
                 dividend_growth = tree.xpath('/html/body/div/div[1]/div[2]/main/div[2]/div/div[2]/div[6]/div/text()')[0].strip()
-                return {"Ticker": ticker, "Price": price, "Yield %": yield_percent, "Annual Dividend": annual_dividend, "Ex Dividend Date": ex_dividend_date, "Frequency": frequency, "Dividend Growth %": dividend_growth}
+                return {
+                    "Ticker": ticker, "Price": price, "Yield %": yield_percent,
+                    "Annual Dividend": annual_dividend, "Ex Dividend Date": ex_dividend_date,
+                    "Frequency": frequency, "Dividend Growth %": dividend_growth
+                }
     except Exception as e:
         print(f"Error fetching data for {ticker}: {e}")
 
-    return {"Ticker": ticker, "Price": "N/A", "Yield %": "N/A", "Annual Dividend": "N/A", "Ex Dividend Date": "N/A", "Frequency": "N/A", "Dividend Growth %": "N/A"}
-
-# Function to get additional stock data
-def get_additional_stock_data(ticker):
-    base_url = "https://www.tradingview.com/symbols/" + ticker
-    try:
-        response = requests.get(base_url)
-        if response.status_code == 200:
-            tree = html.fromstring(response.content)
-            day_1 = tree.xpath('//*[@id="js-category-content"]/div[2]/div/section/div[1]/div[2]/div/div[2]/div/div[2]/button[1]/span/span[2]/text()')[0].strip()
-            day_5 = tree.xpath('//*[@id="js-category-content"]/div[2]/div/section/div[1]/div[2]/div/div[2]/div/div[2]/button[2]/span/span[2]/text()')[0].strip()
-            month_1 = tree.xpath('//*[@id="js-category-content"]/div[2]/div/section/div[1]/div[2]/div/div[2]/div/div[2]/button[3]/span/span[2]/text()')[0].strip()
-            month_6 = tree.xpath('//*[@id="js-category-content"]/div[2]/div/section/div[1]/div[2]/div/div[2]/div/div[2]/button[4]/span/span[2]/text()')[0].strip()
-            ytd = tree.xpath('//*[@id="js-category-content"]/div[2]/div/section/div[1]/div[2]/div/div[2]/div/div[2]/button[5]/span/span[2]/text()')[0].strip()
-            year_1 = tree.xpath('//*[@id="js-category-content"]/div[2]/div/section/div[1]/div[2]/div/div[2]/div/div[2]/button[6]/span/span[2]/text()')[0].strip()
-            year_5 = tree.xpath('//*[@id="js-category-content"]/div[2]/div/section/div[1]/div[2]/div/div[2]/div/div[2]/button[7]/span/span[2]/text()')[0].strip()
-            all_time = tree.xpath('//*[@id="js-category-content"]/div[2]/div/section/div[1]/div[2]/div/div[2]/div/div[2]/button[8]/span/span[2]/text()')[0].strip()
-            return {"1 Day": day_1, "5 Days": day_5, "1 Month": month_1, "6 Month": month_6, "YTD": ytd, "1 Year": year_1, "5 Year": year_5, "All Time": all_time}
-        else:
-            return {"1 Day": "N/A", "5 Days": "N/A", "1 Month": "N/A", "6 Month": "N/A", "YTD": "N/A", "1 Year": "N/A", "5 Year": "N/A", "All Time": "N/A"}
-    except Exception as e:
-        return {"1 Day": "N/A", "5 Days": "N/A", "1 Month": "N/A", "6 Month": "N/A", "YTD": "N/A", "1 Year": "N/A", "5 Year": "N/A"}
+    return {
+        "Ticker": ticker, "Price": "N/A", "Yield %": "N/A",
+        "Annual Dividend": "N/A", "Ex Dividend Date": "N/A",
+        "Frequency": "N/A", "Dividend Growth %": "N/A"
+    }
 
 # Function to get additional ETF data
 def get_additional_etf_data(ticker):
-    base_url = "https://www.tradingview.com/symbols/" + ticker
+    base_url = "https://www.tradingview.com/symbols/AMEX-" + ticker + "/"
     try:
         response = requests.get(base_url)
         if response.status_code == 200:
             tree = html.fromstring(response.content)
-            day_1 = tree.xpath('//button[@class="rangeButtonRed-tEo1hPMj rangeButton-tEo1hPMj selected-tEo1hPMj"]/span[@class="change-tEo1hPMj"]/text()')[0].strip()
-            day_5 = tree.xpath('//button[@class="rangeButtonRed-tEo1hPMj rangeButton-tEo1hPMj"][span/span[text()="5 days"]]/span[@class="change-tEo1hPMj"]/text()')[0].strip()
-            month_1 = tree.xpath('//button[@class="rangeButtonRed-tEo1hPMj rangeButton-tEo1hPMj"][span/span[text()="1 month"]]/span[@class="change-tEo1hPMj"]/text()')[0].strip()
-            month_6 = tree.xpath('//button[@class="rangeButtonGreen-tEo1hPMj rangeButton-tEo1hPMj"][span/span[text()="6 months"]]/span[@class="change-tEo1hPMj"]/text()')[0].strip()
-            ytd = tree.xpath('//button[@class="rangeButtonGreen-tEo1hPMj rangeButton-tEo1hPMj"][span/span[text()="Year to date"]]/span[@class="change-tEo1hPMj"]/text()')[0].strip()
-            year_1 = tree.xpath('//button[@class="rangeButtonGreen-tEo1hPMj rangeButton-tEo1hPMj"][span/span[text()="1 year"]]/span[@class="change-tEo1hPMj"]/text()')[0].strip()
-            year_5 = tree.xpath('//button[@class="rangeButtonGreen-tEo1hPMj rangeButton-tEo1hPMj"][span/span[text()="5 years"]]/span[@class="change-tEo1hPMj"]/text()')[0].strip()
-            all_time = tree.xpath('//button[@class="rangeButtonGreen-tEo1hPMj rangeButton-tEo1hPMj"][span/span[text()="All time"]]/span[@class="change-tEo1hPMj"]/text()')[0].strip()
 
-            return {"1 Day": day_1, "5 Days": day_5, "1 Month": month_1, "6 Month": month_6, "YTD": ytd, "1 Year": year_1, "5 Year": year_5, "All Time": all_time}
+            # Attempt to fetch performance data using updated XPath
+            try:
+                day_1 = tree.xpath('//button[@class="rangeButtonRed-tEo1hPMj rangeButton-tEo1hPMj selected-tEo1hPMj"]/span[@class="change-tEo1hPMj"]/text()')[0].strip()
+            except IndexError:
+                day_1 = "N/A"
+
+            try:
+                day_5 = tree.xpath('//button[@class="rangeButtonRed-tEo1hPMj rangeButton-tEo1hPMj"][span/span[text()="5 days"]]/span[@class="change-tEo1hPMj"]/text()')[0].strip()
+            except IndexError:
+                day_5 = "N/A"
+
+            try:
+                month_1 = tree.xpath('//button[@class="rangeButtonRed-tEo1hPMj rangeButton-tEo1hPMj"][span/span[text()="1 month"]]/span[@class="change-tEo1hPMj"]/text()')[0].strip()
+            except IndexError:
+                month_1 = "N/A"
+
+            try:
+                month_6 = tree.xpath('//button[@class="rangeButtonGreen-tEo1hPMj rangeButton-tEo1hPMj"][span/span[text()="6 months"]]/span[@class="change-tEo1hPMj"]/text()')[0].strip()
+            except IndexError:
+                month_6 = "N/A"
+
+            try:
+                ytd = tree.xpath('//button[@class="rangeButtonGreen-tEo1hPMj rangeButton-tEo1hPMj"][span/span[text()="Year to date"]]/span[@class="change-tEo1hPMj"]/text()')[0].strip()
+            except IndexError:
+                ytd = "N/A"
+
+            try:
+                year_1 = tree.xpath('//button[@class="rangeButtonGreen-tEo1hPMj rangeButton-tEo1hPMj"][span/span[text()="1 year"]]/span[@class="change-tEo1hPMj"]/text()')[0].strip()
+            except IndexError:
+                year_1 = "N/A"
+
+            try:
+                year_5 = tree.xpath('//button[@class="rangeButtonGreen-tEo1hPMj rangeButton-tEo1hPMj"][span/span[text()="5 years"]]/span[@class="change-tEo1hPMj"]/text()')[0].strip()
+            except IndexError:
+                year_5 = "N/A"
+
+            try:
+                all_time = tree.xpath('//button[@class="rangeButtonGreen-tEo1hPMj rangeButton-tEo1hPMj"][span/span[text()="All time"]]/span[@class="change-tEo1hPMj"]/text()')[0].strip()
+            except IndexError:
+                all_time = "N/A"
+
+            return {
+                "1 Day": day_1, "5 Days": day_5, "1 Month": month_1,
+                "6 Month": month_6, "YTD": ytd, "1 Year": year_1,
+                "5 Year": year_5, "All Time": all_time
+            }
         else:
-            return {"1 Day": "N/A", "5 Days": "N/A", "1 Month": "N/A", "6 Month": "N/A", "YTD": "N/A", "1 Year": "N/A", "5 Year": "N/A", "All Time": "N/A"}
+            print(f"Failed to fetch data for {ticker}, status code: {response.status_code}")
+            return {
+                "1 Day": "N/A", "5 Days": "N/A", "1 Month": "N/A",
+                "6 Month": "N/A", "YTD": "N/A", "1 Year": "N/A",
+                "5 Year": "N/A", "All Time": "N/A"
+            }
     except Exception as e:
-        return {"1 Day": "N/A", "5 Days": "N/A", "1 Month": "N/A", "6 Month": "N/A", "YTD": "N/A", "1 Year": "N/A", "5 Year": "N/A"}
+        print(f"Error fetching additional ETF data for {ticker}: {e}")
+        return {
+            "1 Day": "N/A", "5 Days": "N/A", "1 Month": "N/A",
+            "6 Month": "N/A", "YTD": "N/A", "1 Year": "N/A",
+            "5 Year": "N/A", "All Time": "N/A"
+        }
 
 # Streamlit App
 st.title("Stock and ETF Dashboard")
@@ -99,7 +137,7 @@ if tickers:
         additional_etf_data = get_additional_etf_data(ticker.strip())
         if all(value == "N/A" for value in additional_etf_data.values()):
             # If ETF data is not available, fallback to stock data
-            additional_data.append(get_additional_stock_data(ticker.strip()))
+            additional_data.append(get_additional_etf_data(ticker.strip()))
         else:
             additional_data.append(additional_etf_data)
 
